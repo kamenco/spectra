ac428c63d25a3c38695c02ac74bc778c5aa498c5
kamenco <kamen@bgtown.com>
2025-02-06 18:58:55 +0000

    Removed import env because settings.py tries to import it but heroku does not have it.

--- avertise/settings.py
+++ avertise/settings.py
@@ -14,7 +14,6 @@ import os
 import dj_database_url
 from pathlib import Path
 from django.core.exceptions import ImproperlyConfigured
-import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
