83b7e9cd9b890dd6366446f2a89a00c8b315854b
kamenco <kamen@bgtown.com>
2025-02-10 21:30:10 +0000

    Installed AWS CLI and tested uploading and downloading of files.

--- avertise/settings.py
+++ avertise/settings.py
@@ -217,3 +217,20 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Logout redirection
 
 LOGOUT_REDIRECT_URL = '/'
+
+
+# AWS S3 Configuration
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_S3_BUCKET_NAME')
+AWS_S3_REGION_NAME = 'eu-west-1'  # e.g., 'us-east-1', 'eu-west-1'
+
+# Static and Media Files Storage
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
+
+# For storage of sttic files on S3
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/static/'
+
